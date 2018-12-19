068cc02fbc706006335a815817f556cdb89ca60f
zuozewei@techstar.com.cn <zuozewei@hotmail.com>
2018-12-19 11:11:08 +0800

    [+]add extentreport-demo2

---
+++ extentreport-demo2/src/main/java/com/zuozewei/testng/TestClass1.java
@@ -0,0 +1,11 @@
+package com.zuozewei.testng;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+    @Test
+    public void testCase1() {
+        System.out.println("testCase1 run..");
+    }
+}
