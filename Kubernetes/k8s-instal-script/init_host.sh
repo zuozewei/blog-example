790a4449667233f936de2b2c59c132501a0bcd27
zuozewei <zuozewei@techstar.com.cn>
2020-09-22 10:11:25 +0800

    [+] 增加 k8s 部署资料

---
+++ Kubernetes/k8s-instal-script/init_host.sh
@@ -0,0 +1,11 @@
+
+# 只在 master/worker 节点执行
+# 替换 x.x.x.x 为 master 节点的内网IP
+# export 命令只在当前 shell 会话中有效，开启新的 shell 窗口后，如果要继续安装过程，请重新执行此处的 export 命令
+export MASTER_IP=172.16.106.200
+# 替换 apiserver.demo 为 您想要的 dnsName
+export APISERVER_NAME=apiserver.7d.com
+# Kubernetes 容器组所在的网段，该网段安装完成后，由 kubernetes 创建，事先并不存在于您的物理网络中
+export POD_SUBNET=10.100.0.1/16
+echo "${MASTER_IP}    ${APISERVER_NAME}" >> /etc/hosts
+
